a4f2629826191803ea40bb604b45626556e69188
pntoka <pnt17@ic.ac.uk>
2023-03-08 14:44:54 +0000

    Updated web scrape and link selector

--- text_retrieve.py
+++ text_retrieve.py
@@ -69,10 +69,14 @@ class FullTextDownloader:
         elif prefix == self.pub_prefix['Springer']:
             return links[2]  #full text html link but some may be just pdf
         elif prefix == self.pub_prefix['Nature']:
+            for link in links:
+                if link[-3:] == 'pdf':
+                    link_1 = link.replace(link[-4:],'')
+                    return link_1 #html link
             return links[2]  #html link   
         elif prefix == self.pub_prefix['TandF']:
             link = re.sub('/pdf','',links[1])
-            return link
+            return link  #html link
         elif prefix == self.pub_prefix['Science']:
             print('URLs from Science get 403 error')
     
@@ -82,6 +86,7 @@ class FullTextDownloader:
         # driver = webdriver.Firefox(executable_path=r"C:\Users\Piotr\geckodriver.exe", options=options)
         driver = webdriver.Firefox()
         driver.get(link)
+        driver.implicitly_wait(3)
         page = driver.page_source.encode('utf-8')
         # driver.close()
         with open(
@@ -98,7 +103,7 @@ if __name__ == '__main__':
     pub_prefix = {"RSC": "10.1039", "ACS": "10.1021", "Nature":"10.1038", "Science":"10.1126", "Frontiers":"10.3389", "MDPI":"10.3390", "Wiley": "10.1002", "Springer":"10.1007", "TandF":"10.1080", "Elsevier":"10.1016"}
     # save_dir_laptop = r"C:\Users\Piotr\MRes_project\full_texts_test"
     save_dir = "/Users/pnt17/Library/CloudStorage/OneDrive-ImperialCollegeLondon/MRes_project_data/full_text_tests/"
-    my_api_key  = "4c35b0bb7a3b3dd148b4561345bdf7d9"
+    my_api_key  = "" #insert your own API key
     downloader = FullTextDownloader(pub_prefix,my_api_key)
     with open("/Users/pnt17/Library/CloudStorage/OneDrive-ImperialCollegeLondon/MRes_project_data/doi_test.txt",'r') as file:
         for line in file:
@@ -112,7 +117,7 @@ if __name__ == '__main__':
                 link = downloader.link_selector(doi, links)
                 downloader.web_scrape(doi,link,save_dir)
     # doi = "10.1002/chir.23509"
-    # doi = "10.1002/bio.3803"
+    # doi = "10.1080/10667857.2018.1492683"
     # links = downloader.crossref_link(doi)
     # link = downloader.link_selector(doi, links)
     # downloader.web_scrape(doi, link, save_dir)
