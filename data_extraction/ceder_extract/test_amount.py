d623a35c7a11f21bd2961dfc256f5a75dd52a30d
ptoka <pnt17@ic.ac.uk>
2023-05-12 13:14:46 +0100

    Testing mat recognition and mat amount extractor

---
+++ data_extraction/ceder_extract/test_amount.py
@@ -0,0 +1,27 @@
+import json
+from MaterialAmountExtractor import get_materials_amounts
+
+if __name__ == '__main__':
+    with open('results_10_sample.json', 'r') as f:
+        results = json.load(f)
+    
+    paragraphs = []
+    for i in range(len(results)):
+        material_amounts = []
+        for element in results[i]:
+            sentence = element['sentence']
+            materials = []
+            for material in element['all_materials']:
+                materials.append(material['text'])
+            m_m = get_materials_amounts.GetMaterialsAmounts(sentence, materials)
+            material_amounts.append(m_m.final_result())
+        paragraphs.append(material_amounts)
+
+
+    with open('material_amounts_10_sample.json', 'w') as f:
+        json.dump(paragraphs, f, indent=4,sort_keys=True,ensure_ascii=False)
+    
+    # materials_in_sentence = ["3-amino-4-chlorophenylboronic acid","3-aminobenzeneboronic acid", "water"]
+    # sentence = str("3-amino-4-chlorophenylboronic acid (0.3428 g) and 3-aminobenzeneboronic acid (0.2739 g) was dissolved in 20 mL distilled water and sonicated for 2 min, respectively.")
+    # m_m = get_materials_amounts.GetMaterialsAmounts(sentence, materials_in_sentence)
+    # print(m_m.final_result())
