dfa1de0b45d3228efafcb7874cebe587f85ec148
ptoka <pnt17@ic.ac.uk>
2023-05-22 16:28:07 +0100

    More testing

--- data_extraction/ceder_extract/test_amount.py
+++ data_extraction/ceder_extract/test_amount.py
@@ -20,7 +20,12 @@ if __name__ == '__main__':
 
     with open('material_amounts_10_sample.json', 'w') as f:
         json.dump(paragraphs, f, indent=4,sort_keys=True,ensure_ascii=False)
-    
+    # with open('material_amounts_10_sample.json', 'r') as f:
+    #     results = json.load(f)
+    # print(results[0][0])
+    # print(results[0][0]==None)
+
+
     # materials_in_sentence = ["3-amino-4-chlorophenylboronic acid","3-aminobenzeneboronic acid", "water"]
     # sentence = str("3-amino-4-chlorophenylboronic acid (0.3428 g) and 3-aminobenzeneboronic acid (0.2739 g) was dissolved in 20 mL distilled water and sonicated for 2 min, respectively.")
     # m_m = get_materials_amounts.GetMaterialsAmounts(sentence, materials_in_sentence)
--- data_extraction/ceder_extract/test_extract.py
+++ data_extraction/ceder_extract/test_extract.py
@@ -1,23 +1,49 @@
 import json
 from pprint import pprint
-from materials_entity_recognition import MatRecognition
-from materials_entity_recognition import MatIdentification
+# from materials_entity_recognition import MatRecognition
+# from materials_entity_recognition import MatIdentification
+import extraction as ext
 
 if __name__ == '__main__':
     # with open('para_10_sample.txt', 'r') as f:
     #     paras = json.load(f)
-    paras = []
-    dois = []
-    for line in open('para_10_sample.txt', 'r'):
-        paras.append(line.split(':',1)[1].strip())
-        dois.append(line.split(':',1)[0])
+    # paras = []
+    # dois = []
+    # for line in open('para_10_sample.txt', 'r'):
+    #     paras.append(line.split(':',1)[1].strip())
+    #     dois.append(line.split(':',1)[0])
     
-    model_new = MatRecognition()
-    result = model_new.mat_recognize(paras)
-    with open('results_10_sample.json', 'w') as f:
-        json.dump(result, f, indent=4,sort_keys=True,ensure_ascii=False)
-    with open('sample_dois.txt', 'w') as f:
-        for doi in dois:
-            f.write(doi + '\n')
+    # result = ext.extract_materials(paras)
+    with open('results_10_sample.json', 'r') as f:
+        result = json.load(f)
+
+    with open('material_amounts_10_sample.json', 'r') as f:
+        amounts = json.load(f)
+    
+    precursors, all_materials = ext.materials_extraction(result)
+    amount_dict, precursors_dict = ext.amount_compiler(amounts, all_materials, precursors)
+    print(precursors_dict)
+
+    # print(len(amount_dict))
+    # print(amount_dict)
+    # with open('graphs_10_sample.json', 'r') as f:
+    #     graphs = json.load(f)
+    
+    # heating_operations = ext.heating_operation_extraction(graphs)
+    # print(len(heating_operations))
+    # print(heating_operations)
+   
+
+
+    # print(len(precursors))
+    # print(len(all_materials))
+    # model_new = MatRecognition()
+    # result = model_new.mat_recognize(paras)
+    # print('complete')
+    # with open('results_10_sample.json', 'w') as f:
+    #     json.dump(result, f, indent=4,sort_keys=True,ensure_ascii=False)
+    # with open('sample_dois.txt', 'w') as f:
+    #     for doi in dois:
+    #         f.write(doi + '\n')
     
 
