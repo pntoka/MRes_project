28c2211b93f54643f3a8ecb41714c00919081dc7
Piotr Toka <s1749084@sms.ed.ac.uk>
2023-07-04 23:26:50 +0100

    Added functions

--- text_retrieval/paper_filter.py
+++ text_retrieval/paper_filter.py
@@ -1,6 +1,7 @@
 import os
 import json
 from bs4 import BeautifulSoup
+import re
 
 def get_abstract(doi, save_dir):
     with open(os.path.join(save_dir,doi.replace('/', '-')+'.txt'), 'r', encoding='utf-8') as f:
@@ -8,3 +9,21 @@ def get_abstract(doi, save_dir):
     soup = BeautifulSoup(html_xml_str, 'xml')
     abstract = soup.find('abstract')
     return abstract.find('para').get_text()
+
+def abstract_filter(abstract, keywords):
+    for keyword in keywords:
+        if keyword in abstract:
+            return True
+    return False
+
+def get_doi_materials(doi, data_entry):
+    materials  = list(data_entry['all_materials'].keys())
+    return materials
+
+def materials_filter(materials, keywords):
+    for material in materials:
+        for keyword in keywords:
+            if keyword in material:
+                return True
+    return False
+
