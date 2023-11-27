989218e47ce25ea89b4e3748e4f844381aee5274
pntoka <122290997+pntoka@users.noreply.github.com>
2023-11-27 15:19:27 +0000

    Update test_retrieve.py

--- text_retrieval/test_retrieve.py
+++ text_retrieval/test_retrieve.py
@@ -3,7 +3,7 @@ import web_scraper
 if __name__ == '__main__':
     # doi = '10.1007/s00604-017-2367-0'
     doi = '10.1007/s00216-015-9138-8'
-    my_api_key = "4c35b0bb7a3b3dd148b4561345bdf7d9"
+    my_api_key = ""
     downloader = web_scraper.FullTextDownloader(web_scraper.PUB_PREFIX, my_api_key)
     links = downloader.crossref_link(doi)
     link = downloader.link_selector(doi, links)
